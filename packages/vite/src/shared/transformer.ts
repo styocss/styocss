9243dfa620419cd071f4234ac1d861c66d60c8d1
DevilTea <ch870814@gmail.com>
2024-11-06 17:44:20 +0800

    refactor: transformer function

--- packages/vite/src/shared/transformer.ts
+++ packages/vite/src/shared/transformer.ts
@@ -2,11 +2,38 @@ import type { StyoEngine } from '@styocss/core'
 import MagicString from 'magic-string'
 import type { StyoPluginContext } from '../shared/types'
 
+function findFunctionCallPositions(code: string, RE: RegExp) {
+	const functionCallPositions: { fnName: string, start: number, end: number }[] = []
+	let match: RegExpExecArray | null = RE.exec(code)
+
+	while (match != null) {
+		const fnName = match[1]!
+		const start = match.index
+		let end = start + fnName.length
+		let depth = 1
+		let inString: '\'' | '"' | false = false
+		while (depth > 0) {
+			end++
+			if (inString === false && code[end] === '(')
+				depth++
+			else if (inString === false && code[end] === ')')
+				depth--
+			else if (inString === false && (code[end] === '\'' || code[end] === '"'))
+				inString = code[end] as '\'' | '"'
+			else if (inString === code[end])
+				inString = false
+		}
+		functionCallPositions.push({ fnName, start, end })
+		match = RE.exec(code)
+	}
+
+	return functionCallPositions
+}
+
 export function createFunctionCallTransformer(ctx: StyoPluginContext) {
 	// eslint-disable-next-line style/newline-per-chained-call
 	const RE = new RegExp(`\\b(${Object.values(ctx.styoFnNames).map(s => `(${s})`).join('|')})\\(`, 'g')
 
-	// eslint-disable-next-line complexity
 	return async function transformFunctionCalls(code: string, id: string) {
 		if (!ctx.needToTransform(id))
 			return
@@ -14,33 +41,10 @@ export function createFunctionCallTransformer(ctx: StyoPluginContext) {
 		ctx.usages.delete(id)
 
 		// Find all target function calls
-		const functionCallPositions: { fnName: string, start: number, end: number }[] = []
-		let match: RegExpExecArray | null = RE.exec(code)
+		const functionCallPositions = findFunctionCallPositions(code, RE)
 
-		while (match != null) {
-			const fnName = match[1]!
-			const start = match.index
-			let end = start + fnName.length
-			let depth = 1
-			let inString: '\'' | '"' | false = false
-			while (depth > 0) {
-				end++
-				if (inString === false && code[end] === '(')
-					depth++
-				else if (inString === false && code[end] === ')')
-					depth--
-				else if (inString === false && (code[end] === '\'' || code[end] === '"'))
-					inString = code[end] as '\'' | '"'
-				else if (inString === code[end])
-					inString = false
-			}
-			functionCallPositions.push({ fnName, start, end })
-			match = RE.exec(code)
-		}
-
-		if (functionCallPositions.length === 0) {
+		if (functionCallPositions.length === 0)
 			return
-		}
 
 		const usages: (Parameters<StyoEngine['styo']>)[] = []
 		ctx.usages.set(id, usages)
