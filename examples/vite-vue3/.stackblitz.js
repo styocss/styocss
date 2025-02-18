8cef20ec22accdbfc207a02fc01b8b7ce5f809d9
DevilTea <ch870814@gmail.com>
2025-02-18 11:12:07 +0800

    chore(examples): update setup

--- examples/vite-vue3/.stackblitz.js
+++ examples/vite-vue3/.stackblitz.js
@@ -5,7 +5,7 @@ updateDependencyLinksToLatest('./package.json')
 function updateDependencyLinksToLatest(filename) {
 	try {
 		const contents = fs.readFileSync(filename, 'utf-8')
-		const updatedContent = contents.replace(/"workspace:*"/gi, '"latest"')
+		const updatedContent = contents.replace(/"workspace:\*"/gi, '"latest"')
 		fs.writeFileSync(filename, updatedContent)
 	}
 	catch (err) {
